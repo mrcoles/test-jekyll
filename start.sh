b4dbbbf9ff7a62743b7e6c42e3c18b63b5dfb4f3
Peter Coles <peter@mrcoles.com>
2019-02-13 15:27:05 -0500

    got a cloudinary example working

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+JEKYLL_ENV=development bundle exec jekyll serve
+
+# JEKYLL_ENV=development bundle exec jekyll serve &
+# P1=$!
+
+# cd _js && npm run start &
+# P2=$!
+
+# wait $P1 $P2
+
